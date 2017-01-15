e3a58a43f824aaee3cc06dbd64f3be4f86a9259b
ThisIsTyDell <milleb94@gmail.com>
2017-01-15 13:48:43 -0600

    configure routes

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,1 +1,21 @@
-routes.js
+angular
+  .module('myApp')
+  .config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
+    $urlRouterProvider.otherwise('/home')
+  })
