73ce4ba4e19c84a31a9e2a63cdb54d0eaea70552
ThisIsTyDell <milleb94@gmail.com>
2017-01-15 14:26:06 -0600

    configure devise to use usernames

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,13 @@ class ApplicationController < ActionController::Base
 
   def index
     render 'application/index'
-  end 
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
