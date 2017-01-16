0fa79be587fc84d6db5785605cec8625047d94fc
ThisIsTyDell <milleb94@gmail.com>
2017-01-16 17:20:00 -0600

    add name and belongs to to migration

--- db/migrate/20170116225536_create_songs.rb
+++ db/migrate/20170116225536_create_songs.rb
@@ -1,6 +1,8 @@
 class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
+      t.string :name
+      t.belongs_to :user, index: true
 
       t.timestamps
     end
