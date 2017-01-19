06a17e2a7a73d30ccffb3b8d905beb68fef3bd17
ThisIsTyDell <milleb94@gmail.com>
2017-01-19 13:56:00 -0600

    add get request to get list of songs from API

--- app/assets/javascripts/Services/SongsService.js
+++ app/assets/javascripts/Services/SongsService.js
@@ -1,1 +1,9 @@
-SongsService.js
+function SongsService($http) {
+  this.getSongs = function() {
+    return $http.get('/songs');
+  }
+}
+
+angular
+  .module('myApp')
+  .service('SongsService', SongsService);
