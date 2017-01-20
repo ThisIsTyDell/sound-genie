9e24eca33da3d6225fc469088b0c6d73f752e81d
ThisIsTyDell <milleb94@gmail.com>
2017-01-20 16:13:36 -0600

    set controller

--- app/assets/javascripts/directives/CreditsDirective.js
+++ app/assets/javascripts/directives/CreditsDirective.js
@@ -2,6 +2,7 @@ angular
   .module('myApp')
   .directive('availableCredits', function(){
     return {
-      templateUrl: 'views/credits.html'
+      templateUrl: 'views/credits.html',
+      controller: 'AccountCtrl'
     }
   })
