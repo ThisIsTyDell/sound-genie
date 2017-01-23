7d9f7718b5b85385576b7d7c8cf32c735f0871f0
ThisIsTyDell <milleb94@gmail.com>
2017-01-22 21:11:43 -0600

    create show and create actions in song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,19 @@ class SongsController < ApplicationController
     songs = Song.all
     render json: songs
   end
+
   def show
     song = Song.find(params[:id])
     render json: song
   end
+
+  def create
+    song = current_user.songs.create(song_params)
+  end
+
+  private
+    def song_params
+      params.require(:song).permit(:name, :asset)
+    end
   
 end
