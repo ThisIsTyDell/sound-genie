c86a2eedd87f7938e08ef9d5d7284a0f4c938c37
ThisIsTyDell <milleb94@gmail.com>
2017-01-22 20:58:27 -0600

    add has attached file for paperclip

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,10 @@
 class Song < ApplicationRecord
   belongs_to :user
+  has_attached_file :asset, styles: {thumb: "100x100#"}
+
+  before_post_process :skip_for_audio
+
+  def skip_for_audio
+    ! %w(audio/ogg application/ogg).include?(asset_content_type)
+  end
 end
